8b5caa979cf9f2a0a3e244563f4fd9a0d3b1ed0f
prisha12341 <93686097+prisha1234@users.noreply.github.com>
2021-11-20 00:34:04 +0000

    from random import randint import time #creat a list list = ["rock", "paper ", "scissors"]
    #a sign a random play to the computer
    computer = list[randint(0,2)]
    
    #set player to false
    player = False
    while players == False:

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from random import randint
 import time 
+#creat a list
+list = ["rock", "paper ", "scissors"]
+
+#a sign a random play to the computer 
+computer = list[randint(0,2)]
+
+#set player to false
+player = False 
+while players == False:
