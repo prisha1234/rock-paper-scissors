dfe2ae48bc69fae39de73d4ef326d99ee5815b56
prisha12341 <93686097+prisha1234@users.noreply.github.com>
2021-11-30 00:31:01 +0000

    did 10 more lines yayyyyy

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from random import randint
 import time 
 #creat a list
 list = ["rock", "paper ", "scissors"]
-
 #a sign a random play to the computer 
 computer = list[randint(0,2)]
-
-#set player to false
+#set player to false player not very
 player = False 
-while players == False:
+while player == False:
+  #player to true 
+  player = input("rock,paper,scissors ?")
+  if player == computer:
+   print ("tie")
+  elif player == "rock":
+    if computer == "paper":
+      print("computer chooses ",computer)
+      time.sleep(2)
+      print ("you lose :( ", computer,"covers",player)
+    else:
+        print("computer chooses ",computer)
+      time.sleep(2)
+      print ("you win yayyy :D !!!!!!!! ", player ,"breaks",computer)
+  
